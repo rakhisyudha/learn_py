2c6f208f380db18b9f212b914ba54239fcae199d
Rakhis Yudha <rakhisyuda@gmail.com>
2024-09-17 15:23:12 +0700

    list

---
+++ list.py
@@ -0,0 +1,9 @@
+'''name = ['Rakhis', 'Haikal', 'Farhan', 'Ezot', 'Teza', 'Faisal']
+print(name[2:4])'''
+
+numbers = [1, 2, 3, 4, 10, 5, 2, 2]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
